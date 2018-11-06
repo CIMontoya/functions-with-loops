4cde278f986ab804fd26e1d8c7ec6aa037468300
James Schultz <james.schultz@A1-10110010.local>
2018-11-06 09:00:24 -0700

    initial commit ❄️

---
+++ functionsWithLoops.js
@@ -0,0 +1,72 @@
+
+
+var heros = {
+  superheroes: [ 
+      {
+          name: "Spider-man",
+          alterEgo: {
+              first: "Peter",
+              last: "Parker"
+          },
+          age: 15,
+          address: {
+              country: "USA",
+              city: "New York"
+          },
+          favoriteColors: ["blue", "red"]
+      },
+      {
+          name: "Batman",
+          alterEgo: {
+              first: "Bruce",
+              last: "Wayne"
+          },
+          age: 32,
+          address: {
+              country: "USA",
+              city: "Gotham"
+          },
+          favoriteColors: ["black", "yellow"]
+      },
+      {
+          name: "Superman",
+          alterEgo: {
+            first: "Clark",
+            last: "Kent"
+          },
+          age: 35,
+          address: {
+              country: "USA",
+              city: "Metropolis"
+          },
+          favoriteColors: ["blue","red"]
+      },
+      {
+          name: "Flash",
+          alterEgo: {
+              first: "Barry",
+              last: "Allen"
+          },
+          age: 28,
+          address: {
+              country: "USA",
+              city: "Central City"
+          },
+          favoriteColors: ["red", "yellow"]
+      }
+  ]
+}
+
+
+// 1. Write a function that loops through the superheros and list out each name of all the superheroes. BONUS try pushing them to an array you created.
+
+
+
+// 2. Write a function that loops through the superheros and finds each persons the age... HINT this might look like the function you wrote above. BONUS try pushing them to an array!
+
+
+
+// 3. Write a function that loops through the superheroes and finds each person's first and last name. BOUNS try and push into an array!
+
+
+//4. Write a function that loops through the superheroes and finds each hero's favorite color. This will be a nested for loop. BONUS Try to match each hero's name with their fav colors and print it out in a string.
